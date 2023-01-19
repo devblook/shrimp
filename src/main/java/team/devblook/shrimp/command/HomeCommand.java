afa41b75a444bae65b9441f82a69e952cda1aaaf
Call4han <ccasarider@gmail.com>
2023-01-19 16:43:55 -0500

    fix(config): fix problems with home commands.

--- src/main/java/team/devblook/shrimp/command/HomeCommand.java
+++ src/main/java/team/devblook/shrimp/command/HomeCommand.java
@@ -7,10 +7,7 @@ import me.fixeddev.commandflow.annotated.annotation.OptArg;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -19,8 +16,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import team.devblook.shrimp.util.BukkitConfiguration;
 import team.unnamed.inject.InjectAll;
+
 import javax.inject.Named;
-import java.util.ArrayList;
 import java.util.Set;
 
 @InjectAll
@@ -35,58 +32,36 @@ public class HomeCommand implements CommandClass {
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
     @Command(names = "")
-    public void mainCommand(@Sender Player player,@OptArg String nameHome) {
+    public void mainCommand(@Sender Player player, @OptArg String nameHome) {
         FileConfiguration playersConfiguration = players.get();
-        Set<String> keys = playersConfiguration.getConfigurationSection(player.getName()).getKeys(false);
-        if (keys==null){
+        Set<String> keys = playersConfiguration.getConfigurationSection(player.getName() + ".home").getKeys(false);
+        if (keys.isEmpty() || keys == null) {
             Audience audience = (Audience) player;
             audience.sendMessage(Component.text("You don't have any home").color(NamedTextColor.RED));
             return;
         }
-        keys.forEach(key -> {
-            if (key.equalsIgnoreCase(nameHome)){
-                World world = Bukkit.getWorld(playersConfiguration.getString(player.getName()+"."+key+".world"));
-                double x = playersConfiguration.getDouble(player.getName()+"."+key+".x");
-                double y = playersConfiguration.getDouble(player.getName()+"."+key+".y");
-                double z = playersConfiguration.getDouble(player.getName()+"."+key+".z");
-                float yaw = (float) playersConfiguration.getDouble(player.getName()+"."+key+".yaw");
-                float pitch = (float) playersConfiguration.getDouble(player.getName()+"."+key+".pitch");
-                Location location = new Location(world,x,y,z,yaw,pitch);
-                PaperLib.teleportAsync(player,location);
-                Audience audience = (Audience) player;
-                audience.sendMessage(Component.text("Teleported to "+key).color(NamedTextColor.GREEN));
+        if (keys.stream().anyMatch(key -> key.equalsIgnoreCase(nameHome))) {
+            for (String homesKey : keys) {
+                if (homesKey.equalsIgnoreCase(nameHome)) {
+                    World world = Bukkit.getWorld(playersConfiguration.getString(player.getName() + ".home." + homesKey + ".world"));
+                    double x = playersConfiguration.getDouble(player.getName() + ".home." + homesKey + ".x");
+                    double y = playersConfiguration.getDouble(player.getName() + ".home." + homesKey + ".y");
+                    double z = playersConfiguration.getDouble(player.getName() + ".home." + homesKey + ".z");
+                    float yaw = (float) playersConfiguration.getDouble(player.getName() + ".home." + homesKey + ".yaw");
+                    float pitch = (float) playersConfiguration.getDouble(player.getName() + ".home." + homesKey + ".pitch");
+                    Location location = new Location(world, x, y, z, yaw, pitch);
+                    PaperLib.teleportAsync(player, location);
+                    return;
+                }
             }
-        });
-    }
-
-    @Command(names = "home", permission = "shrimp.home")
-    public void toHomeCommand(@Sender Player player, @OptArg String home){
-        if (home == null){
-            player.sendMessage("You must specify a home");
             return;
         }
-        FileConfiguration config = players.get();
-        ArrayList<String> keys = new ArrayList<>(config.getKeys(false));
-        keys.forEach(k->{
-            if(k.equalsIgnoreCase(player.getName())){
-                Set<String> subKeys = players.get().getConfigurationSection(k).getKeys(false);
-                subKeys.forEach(subKey->{
-                    if (subKey.equalsIgnoreCase(home)){
-                        World world = Bukkit.getWorld(config.getString(k + "." + subKey + ".world"));
-                        double x = config.getDouble(k + "." + subKey + ".x");
-                        double y = config.getDouble(k + "." + subKey + ".y");
-                        double z = config.getDouble(k + "." + subKey + ".z");
-                        float yaw = (float) config.getDouble(k + "." + subKey + ".yaw");
-                        float pitch = (float) config.getDouble(k + "." + subKey + ".pitch");
-                        Location location = new Location(world, x, y, z, yaw, pitch);
-                        player.teleport(location);
-                        player.sendMessage("Teleported to home");
-                    }
-                });
-            }
-        });
+        player.sendMessage("You don't have any home with this name");
+        return;
+
 
     }
+
 
 }
 
--- src/main/java/team/devblook/shrimp/command/SetHomeCommand.java
+++ src/main/java/team/devblook/shrimp/command/SetHomeCommand.java
@@ -1,20 +1,12 @@
 package team.devblook.shrimp.command;
 
-import jdk.jshell.execution.Util;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.annotated.annotation.OptArg;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 import team.devblook.shrimp.util.BukkitConfiguration;
 import team.devblook.shrimp.util.PlayerData;
 import team.unnamed.inject.InjectAll;
@@ -31,33 +23,37 @@ public class SetHomeCommand implements CommandClass {
 
     private BukkitConfiguration settings;
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+
     @Command(names = "")
     public void homeCommandEmpty(@Sender Player player, @OptArg("") String nameHome) {
         FileConfiguration playerConfig = players.get();
         FileConfiguration settingsConfig = settings.get();
-        if (nameHome.isEmpty()||nameHome.equals(" ")) {
-            player.sendMessage("Debes poner un nombre a tu home");
+        PlayerData playerData = new PlayerData();
+        if (nameHome.isEmpty() || nameHome.equals(" ")) {
+            player.sendMessage("You must put a name to your home");
             return;
         }
         Set<String> keysPlayer = playerConfig.getKeys(false);
-        if (keysPlayer.isEmpty()){
-            player.sendMessage("empty");
+        if (keysPlayer.isEmpty()) {
+            playerData.setDataPlayer(player, playerConfig, nameHome);
+            players.save();
+            player.sendMessage("You have created your first home");
             return;
         }
-        keysPlayer.forEach(key->{
-            if (key.equals(player.getName())){
-                Set<String> keysHome = playerConfig.getConfigurationSection(key+".home").getKeys(false);
-                if (keysHome.size()+1>settingsConfig.getInt("max-home-amount")){
-                    player.sendMessage("Has alcanzado el limite de homes");
+        keysPlayer.forEach(key -> {
+            if (key.equals(player.getName())) {
+                Set<String> keysHome = playerConfig.getConfigurationSection(key + ".home").getKeys(false);
+                if (keysHome.size() > settingsConfig.getInt("max-home-amount")) {
+                    player.sendMessage("You have reached the limit of houses");
                     return;
                 }
-                if (keysHome.stream().anyMatch(keyHome->keyHome.equals(nameHome))){
-                    player.sendMessage("Ya tienes un home con ese nombre");
+                if (keysHome.stream().anyMatch(keyHome -> keyHome.equals(nameHome))) {
+                    player.sendMessage("You already have a home with that namee");
                     return;
                 }
-                PlayerData playerData = new PlayerData();
-                playerData.setDataPlayer(player,playerConfig,nameHome);
-                player.sendMessage("Has creado un home con el nombre "+nameHome);
+
+                playerData.setDataPlayer(player, playerConfig, nameHome);
+                player.sendMessage("You have created a home with the name " + nameHome);
                 players.save();
                 return;
             }
