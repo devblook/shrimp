524b5bef32cf579c91c50e2cb973177f94e5ddd2
Call4han <ccasarider@gmail.com>
2023-01-20 20:23:25 -0500

    refactor(space): added space for more readability.

--- src/main/java/team/devblook/shrimp/command/SetHomeCommand.java
+++ src/main/java/team/devblook/shrimp/command/SetHomeCommand.java
@@ -7,8 +7,8 @@ import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import team.devblook.shrimp.data.HomeData;
 import team.devblook.shrimp.util.BukkitConfiguration;
-import team.devblook.shrimp.util.HomeData;
 import team.unnamed.inject.InjectAll;
 
 import javax.inject.Named;
@@ -36,7 +36,7 @@ public class SetHomeCommand implements CommandClass {
         }
         Set<String> keysPlayer = playerConfig.getKeys(false);
         if (keysPlayer.isEmpty()) {
-            homeData.setDataPlayer(player,playerConfig,nameHome);
+            homeData.setDataPlayer(player, playerConfig, nameHome);
             player.sendMessage("You have created your first home");
             return;
         }
@@ -52,7 +52,7 @@ public class SetHomeCommand implements CommandClass {
                     return;
                 }
 
-                homeData.setDataPlayer(player,playerConfig,nameHome);
+                homeData.setDataPlayer(player, playerConfig, nameHome);
                 player.sendMessage("You have created a home with the name " + nameHome);
                 return;
             }
