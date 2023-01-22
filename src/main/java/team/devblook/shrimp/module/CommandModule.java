11f8191e38a40a94bc710dd00448bf138f2d7fa1
Jonathan Narvaez <89610430+jonakls@users.noreply.github.com>
2023-01-22 03:42:16 -0500

    refactor(command): move classes to home package in commands package

--- src/main/java/team/devblook/shrimp/module/CommandModule.java
+++ src/main/java/team/devblook/shrimp/module/CommandModule.java
@@ -1,10 +1,10 @@
 package team.devblook.shrimp.module;
 
 import me.fixeddev.commandflow.annotated.CommandClass;
-import team.devblook.shrimp.command.DeleteHomeCommand;
-import team.devblook.shrimp.command.HomeCommand;
+import team.devblook.shrimp.command.home.DeleteHomeCommand;
+import team.devblook.shrimp.command.home.HomeCommand;
 import team.devblook.shrimp.command.MainCommand;
-import team.devblook.shrimp.command.SetHomeCommand;
+import team.devblook.shrimp.command.home.SetHomeCommand;
 import team.unnamed.inject.AbstractModule;
 
 public class CommandModule extends AbstractModule {
