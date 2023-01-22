fccde3fe791df276ada63473b96cbff42fece79c
Jonathan Narvaez <89610430+jonakls@users.noreply.github.com>
2023-01-22 03:40:04 -0500

    feat(handler): implement handler of user for save all information when plugin off

--- src/main/java/team/devblook/shrimp/Shrimp.java
+++ src/main/java/team/devblook/shrimp/Shrimp.java
@@ -3,19 +3,24 @@ package team.devblook.shrimp;
 import org.bukkit.plugin.java.JavaPlugin;
 import team.devblook.shrimp.module.PluginModule;
 import team.devblook.shrimp.service.Service;
+import team.devblook.shrimp.user.UserHandler;
 import team.unnamed.inject.Injector;
 
 import javax.inject.Inject;
 import java.util.Set;
 
 public class Shrimp extends JavaPlugin {
+
     @Inject
     private Set<Service> services;
+    @Inject
+    private UserHandler userHandler;
 
     @Override
     public void onLoad() {
         Injector injector = Injector.create(new PluginModule(this));
         injector.injectMembers(this);
+
     }
 
     @Override
@@ -26,6 +31,8 @@ public class Shrimp extends JavaPlugin {
     @Override
     public void onDisable() {
         services.forEach(Service::stop);
+
+        userHandler.saveAll();
     }
 }
 
