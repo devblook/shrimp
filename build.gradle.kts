9d6bb501092ecd97bba39188ca60fc19b05d48af
Jonathan Narvaez <89610430+jonakls@users.noreply.github.com>
2023-01-22 03:34:21 -0500

    build: change dependency api from spigot to paper api
    add gui api from triumohteam/gui for menus ui interfaces
    
    add properties of project with version and group content

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,23 +7,19 @@ repositories {
     mavenCentral()
     mavenLocal()
     maven("https://repo.unnamed.team/repository/unnamed-public/")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 
 }
-version = 1.0
+
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
-    implementation("net.kyori:adventure-text-minimessage:4.12.0")
-    implementation("team.unnamed:inject:1.0.1")
-    implementation("me.fixeddev:commandflow-bukkit:0.5.2")
-    implementation("io.papermc:paperlib:1.0.7")
-}
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    implementation("com.github.cryptomorin:XSeries:9.2.0") {
+        isTransitive = false
     }
+    implementation("org.mongodb:mongodb-driver-sync:4.8.1")
+    implementation("team.unnamed:inject:1.0.1")
+    implementation("dev.triumphteam:triumph-gui:3.1.2")
+    implementation("me.fixeddev:commandflow-bukkit:0.5.2")
 }
 
 tasks {
@@ -31,5 +27,15 @@ tasks {
         archiveBaseName.set("Shrimp")
         archiveVersion.set("${project.version}")
         archiveClassifier.set("")
+
+        // Relocations
+        relocate("dev.triumphteam.gui", "${rootProject.group}.shrimp.libs.gui")
+        relocate("me.fixeddev", "${rootProject.group}.shrimp.libs.commandflow")
+        relocate("team.unnamed.inject", "${rootProject.group}.shrimp.libs.inject")
+        relocate("org.mongodb.mongodb-driver-sync", "${rootProject.group}.shrimp.libs.commandflow")
+    }
+
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
