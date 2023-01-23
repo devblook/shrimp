cc85a7b5f1fb70d46c87a232af1f9e4f2f116609
Call4han <ccasarider@gmail.com>
2023-01-22 20:07:40 -0500

    fix(listhomes): the problem that did not return the list of homes was corrected.

--- src/main/java/team/devblook/shrimp/command/MainCommand.java
+++ src/main/java/team/devblook/shrimp/command/MainCommand.java
@@ -3,19 +3,20 @@ package team.devblook.shrimp.command;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
-import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import team.devblook.shrimp.home.Home;
+import team.devblook.shrimp.user.User;
+import team.devblook.shrimp.user.UserHandler;
 import team.devblook.shrimp.util.BukkitConfiguration;
 import team.unnamed.inject.InjectAll;
 
 import javax.inject.Named;
-import java.util.Set;
+import java.util.Collection;
 
 @InjectAll
 @Command(names = {"shrimp"})
@@ -27,36 +28,38 @@ public class MainCommand implements CommandClass {
     @Named("players")
     private BukkitConfiguration players;
 
+    private UserHandler userHandler;
+
     @Command(names = "list", permission = "shrimp.list")
     public void getHomesListCommand(@Sender Player player) {
-        FileConfiguration playersConfiguration = players.get();
-        Set<String> keys = playersConfiguration.getKeys(false);
-        if (!keys.contains(player.getName())) {
-            player.sendMessage("You don't have any homes");
+        User user = userHandler.get(player.getUniqueId().toString());
+        if (user == null) {
+            throw new IllegalStateException("User is null");
+        }
+        Collection<Home> homeCollection = user.homes();
+        if (homeCollection.isEmpty()) {
+            player.sendMessage("You dont have homes");
             return;
         }
-        Set<String> homes = playersConfiguration.getConfigurationSection(player.getName() + ".home").getKeys(false);
         Component component = Component.text("Homes: ").color(NamedTextColor.GOLD);
-        for (String home : homes) {
+        for (Home home : homeCollection) {
+
             Component homeComponent = Component
-                    .text(home + "  ")
+                    .text(home.name() + "  ")
                     .color(NamedTextColor.AQUA)
                     .decoration(TextDecoration.ITALIC, false)
-                    .hoverEvent(HoverEvent.showText(Component.text("Click to teleport to " + home)
+                    .hoverEvent(HoverEvent.showText(Component.text("Click to teleport to " + home.name())
                             .color(NamedTextColor.GOLD)))
-                    .clickEvent(ClickEvent.runCommand("/shrimp:home " + home));
+                    .clickEvent(ClickEvent.runCommand("/shrimp:home " + home.name()));
             component = component.append(homeComponent);
-
         }
-        Audience audience = (Audience) player;
-        audience.sendMessage(component);
+        player.sendMessage(component);
 
 
     }
 
     @Command(names = "reload", permission = "shrimp.op")
     public void reloadCommand(@Sender Player player) {
-        player.sendMessage("?");
         players.reload();
         settings.reload();
         messages.reload();
