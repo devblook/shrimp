0cd6ba4fe7f40eb7f20f99b9b10c486642d217e4
Call4han <ccasarider@gmail.com>
2023-11-21 17:35:51 -0500

    fix: create multiple documents with the same id, now only create one.

--- src/main/java/team/devblook/shrimp/storage/cloud/MongoStorage.java
+++ src/main/java/team/devblook/shrimp/storage/cloud/MongoStorage.java
@@ -5,9 +5,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bson.Document;
+import org.slf4j.Logger;
 import team.devblook.shrimp.Shrimp;
 import team.devblook.shrimp.storage.Storage;
 import team.devblook.shrimp.user.User;
@@ -17,68 +16,68 @@ import javax.inject.Inject;
 import java.util.concurrent.CompletableFuture;
 
 public class MongoStorage implements Storage {
-    private final String host;
+  private final String host;
+  private final String port;
+  private final String user;
+  private final String password;
+  private final String database;
+  private final String collection;
+  private MongoClient mongoClient;
+  private final Shrimp plugin;
+  private final Logger logger;
+  private static final Gson gson = new Gson();
 
-    private final String port;
+  @Inject
+  public MongoStorage(BukkitConfiguration settings, Shrimp plugin) {
+    this.plugin = plugin;
+    this.logger = plugin.getSLF4JLogger();
+    this.host = settings.get().getString("MONGODB.host");
+    this.port = settings.get().getString("MONGODB.port");
+    this.database = settings.get().getString("MONGODB.database");
+    this.user = settings.get().getString("MONGODB.username");
+    this.password = settings.get().getString("MONGODB.password");
+    this.collection = settings.get().getString("MONGODB.collection");
+  }
 
-    private final String user;
+  @Override
+  public void connect() {
+    CompletableFuture.runAsync(() -> {
+      this.mongoClient = MongoClients.create("mongodb+srv://" + this.user + ":" + this.password + "@" + this.host);
+      this.logger.info("MongoDB connected");
+    }).whenComplete((aVoid, throwable) -> {
+      if (throwable != null) {
+        this.logger.error("MongoDB connection failed", throwable);
+      }
+    });
+  }
 
-    private final String password;
+  @Override
+  public void save(User user) {
+    CompletableFuture.runAsync(() -> {
+      MongoDatabase mongoDatabase = this.mongoClient.getDatabase(this.database);
+      MongoCollection<Document> collection = mongoDatabase.getCollection(this.collection);
+      if (collection.find(new Document("id", user.id())).first() != null) {
+        collection.updateOne(new Document("id", user.id()), new Document("$set", Document.parse(gson.toJson(user))));
+        return;
+      }
+      collection.insertOne(Document.parse(new Gson().toJson(user)));
+    }).whenComplete((aVoid, throwable) -> {
+      if (throwable != null) {
+        this.logger.error("MongoDB save failed", throwable);
+      }
+    });
 
-    private final String database;
+  }
 
-    private final String collection;
-    private MongoClient mongoClient;
-    private final Shrimp plugin;
-
-    @Inject
-    public MongoStorage(BukkitConfiguration settings, Shrimp plugin) {
-        this.plugin = plugin;
-        this.host = settings.get().getString("MONGODB.host");
-        this.port = settings.get().getString("MONGODB.port");
-        this.database = settings.get().getString("MONGODB.database");
-        this.user = settings.get().getString("MONGODB.username");
-        this.password = settings.get().getString("MONGODB.password");
-        this.collection = settings.get().getString("MONGODB.collection");
+  @Override
+  public User find(String id) {
+    MongoDatabase mongoDatabase = this.mongoClient.getDatabase(this.database);
+    MongoCollection<Document> collection = mongoDatabase.getCollection(this.collection);
+    Document document = collection.find(new Document("id", id)).first();
+    if (document == null) {
+      return null;
     }
+    return gson.fromJson(document.toJson(), User.class);
+  }
 
-    @Override
-    public void connect() {
-        CompletableFuture.runAsync(() -> {
-            try {
-                this.mongoClient = MongoClients.create("mongodb+srv://" + user + ":" + password + "@" + host);
-                plugin.getComponentLogger().info(Component.text("MongoDB connected").color(NamedTextColor.GREEN));
-                plugin.getLogger().info("Mongo is" + mongoClient);
-
-            } catch (Exception e) {
-                plugin.getComponentLogger().info(Component.text("MongoDB connection failed").color(NamedTextColor.RED));
-                e.printStackTrace();
-            }
-
-        }).join();
-    }
-
-    @Override
-    public void save(User user) {
-        CompletableFuture.runAsync(() -> {
-            MongoDatabase mongoDatabase = this.mongoClient.getDatabase(this.database);
-            MongoCollection<Document> collection = mongoDatabase.getCollection(this.collection);
-            collection.insertOne(Document.parse(new Gson().toJson(user)));
-            plugin.getLogger().info("ez");
-        }).join();
-
-    }
-
-    @Override
-    public User find(String id) {
-        CompletableFuture.supplyAsync(() -> {
-            MongoDatabase mongoDatabase = this.mongoClient.getDatabase(this.database);
-            MongoCollection<Document> collection = mongoDatabase.getCollection(this.collection);
-            Document document = collection.find(new Document("id", id)).first();
-            plugin.getLogger().info("gg");
-            if (document == null) return null;
-            return new Gson().fromJson(document.toJson(), User.class);
-        }).join();
-        return null;
-    }
 }
