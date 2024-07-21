258f5fecb7d76c3ffcc87fdcbb30e06f3f659225
jonakls <asmot54@hotmail.com>
2024-07-21 18:20:04 -0500

    build(dependency): update and add dependencies for new version of shrimp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,54 @@
 plugins {
-    id("java")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
+  id("java")
+  alias(libs.plugins.shadow)
+  alias(libs.plugins.paperPlugin)
 }
 
 repositories {
-    mavenCentral()
-    mavenLocal()
-    maven("https://repo.unnamed.team/repository/unnamed-public/")
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.triumphteam.dev/snapshots/")
+  mavenCentral()
+  mavenLocal()
+  maven("https://repo.unnamed.team/repository/unnamed-public/")
+  maven("https://repo.papermc.io/repository/maven-public/")
+  maven("https://repo.triumphteam.dev/snapshots/")
 
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-    compileOnly("org.mongodb:mongodb-driver-sync:4.8.1")
-    implementation("team.unnamed:inject:1.0.1")
-    implementation("dev.triumphteam:triumph-cmd-bukkit:2.0.0-SNAPSHOT")
+
+  compileOnly(libs.paper)
+
+  implementation(libs.hikari)
+  implementation(libs.mongo)
+
+  implementation(libs.inject)
+  implementation(libs.triumphcmd)
+
+  implementation(libs.lombok)
+  annotationProcessor(libs.lombok)
+
+  testCompileOnly(libs.inject)
+  testAnnotationProcessor(libs.lombok)
+
+  testImplementation(libs.jupiterApi)
+  testRuntimeOnly(libs.jupiterEngine)
 }
 
 tasks {
-    shadowJar {
-        archiveBaseName.set("Shrimp")
-        archiveVersion.set("${project.version}")
-        archiveClassifier.set("BETA")
+  shadowJar {
+    archiveBaseName.set("Shrimp")
+    archiveVersion.set("${project.version}")
+    archiveClassifier.set("BETA")
 
-        // Relocations
-        relocate("dev.triumphteam.cmd", "${rootProject.group}.shrimp.libs.command")
-        relocate("team.unnamed.inject", "${rootProject.group}.shrimp.libs.inject")
-    }
+    // Relocations
+    relocate("dev.triumphteam", "${rootProject.group}.shrimp.libs.command")
+    relocate("team.unnamed", "${rootProject.group}.shrimp.libs.inject")
+    relocate("org.mongodb", "${rootProject.group}.shrimp.libs.mongodb")
+    relocate("com.zaxxer", "${rootProject.group}.shrimp.libs.hikari")
+    relocate("org.projectlombok", "${rootProject.group}.shrimp.libs.bukkit")
+  }
 
-    java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-    }
+  java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+  }
 }
-bukkit {
-    main = "team.devblook.shrimp.Shrimp"
-    name = "Shrimp"
-    version = project.version.toString()
-    apiVersion = "1.17"
-    libraries = listOf("org.mongodb:mongodb-driver-sync:4.8.1")
-    authors = listOf("Call4han")
 
-}
