06c3298e3b614ef56eae16614c8f665f98bb702a
jonakls <asmot54@hotmail.com>
2024-07-30 01:31:34 -0500

    feat(module): install all modules

--- src/main/java/team/devblook/shrimp/inject/PluginModule.java
+++ src/main/java/team/devblook/shrimp/inject/PluginModule.java
@@ -2,6 +2,9 @@ package team.devblook.shrimp.inject;
 
 import org.bukkit.plugin.Plugin;
 import team.devblook.shrimp.inject.command.CommandModule;
+import team.devblook.shrimp.inject.configuration.ConfigurationModule;
+import team.devblook.shrimp.inject.database.DatabaseModule;
+import team.devblook.shrimp.inject.listener.ListenerModule;
 import team.devblook.shrimp.inject.repository.RepositoryModule;
 import team.devblook.shrimp.inject.service.ServiceModule;
 import team.unnamed.inject.AbstractModule;
@@ -16,10 +19,13 @@ public class PluginModule extends AbstractModule {
 
   @Override
   protected void configure() {
-    bind(Plugin.class).toInstance(plugin);
+    bind(Plugin.class).toInstance(this.plugin);
 
+    this.install(new ConfigurationModule());
     this.install(new CommandModule());
     this.install(new RepositoryModule());
     this.install(new ServiceModule());
+    this.install(new DatabaseModule());
+    this.install(new ListenerModule());
   }
 }
