548a90931e893db4b87a31f4e8e89cc2490919dd
jonakls <asmot54@hotmail.com>
2024-07-29 20:54:24 -0500

    feat(command): add commands of home model

--- src/main/java/team/devblook/shrimp/command/home/HomeCommand.java
+++ src/main/java/team/devblook/shrimp/command/home/HomeCommand.java
@@ -1,8 +1,16 @@
 package team.devblook.shrimp.command.home;
 
+import org.bukkit.command.CommandSender;
+import team.devblook.shrimp.model.HomeModel;
 import team.unnamed.commandflow.annotated.CommandClass;
 import team.unnamed.commandflow.annotated.annotation.Command;
+import team.unnamed.commandflow.annotated.annotation.Sender;
 
 @Command(names = "home")
 public class HomeCommand implements CommandClass {
+
+  @Command(names = "")
+  public void home(@Sender CommandSender sender, HomeModel home) {
+    sender.sendMessage("Teleporting to home " + home.getName());
+  }
 }
