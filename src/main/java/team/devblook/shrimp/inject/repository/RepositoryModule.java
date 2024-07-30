7281a948e376c09750a396d2020ce888d63f4003
jonakls <asmot54@hotmail.com>
2024-07-30 01:31:24 -0500

    feat(module): add system for repository

--- src/main/java/team/devblook/shrimp/inject/repository/RepositoryModule.java
+++ src/main/java/team/devblook/shrimp/inject/repository/RepositoryModule.java
@@ -1,11 +1,16 @@
 package team.devblook.shrimp.inject.repository;
 
+import team.devblook.shrimp.model.UserModel;
+import team.devblook.shrimp.repository.TypeObjectRepository;
+import team.devblook.shrimp.repository.TypeRepository;
 import team.unnamed.inject.AbstractModule;
+import team.unnamed.inject.key.TypeReference;
 
 public class RepositoryModule extends AbstractModule {
 
   @Override
   protected void configure() {
-
+    this.bind(new TypeReference<TypeRepository<UserModel>>() { })
+      .toInstance(new TypeObjectRepository<>());
   }
 }
