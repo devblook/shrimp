660aee5565d5decc9d6eb982c1c6e45e9a8ca1b2
jonakls <asmot54@hotmail.com>
2024-07-29 20:54:57 -0500

    feat(service): add internal services

--- src/main/java/team/devblook/shrimp/ShrimpPlugin.java
+++ src/main/java/team/devblook/shrimp/ShrimpPlugin.java
@@ -4,9 +4,16 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import team.devblook.shrimp.inject.PluginModule;
+import team.devblook.shrimp.service.Service;
+import team.unnamed.inject.Inject;
 import team.unnamed.inject.Injector;
 
+import java.util.Set;
+
 public class ShrimpPlugin extends JavaPlugin {
+
+  @Inject
+  private Set<Service> services;
 
   @Override
   public void onLoad() {
@@ -16,11 +23,16 @@ public class ShrimpPlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    this.getComponentLogger().info(Component.text("Initializing ShrimpPlugin", TextColor.fromHexString("#6aafff")));
+    this.getComponentLogger()
+      .info(Component.text("Initializing ShrimpPlugin", TextColor.fromHexString("#6aafff")));
+
+    this.services.forEach(Service::start);
   }
 
   @Override
   public void onDisable() {
+
+    this.services.forEach(Service::stop);
   }
 }
 
