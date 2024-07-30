d7ff332a3290551080aa033dfa82800f521792ca
jonakls <asmot54@hotmail.com>
2024-07-30 01:27:54 -0500

    feat(command): add module for home command part

--- src/main/java/team/devblook/shrimp/command/module/HomePartModule.java
+++ src/main/java/team/devblook/shrimp/command/module/HomePartModule.java
@@ -1,15 +1,19 @@
 package team.devblook.shrimp.command.module;
 
+import team.devblook.shrimp.command.part.HomeListPart;
+import team.devblook.shrimp.model.HomeModel;
 import team.unnamed.commandflow.annotated.part.AbstractModule;
+import team.unnamed.inject.Inject;
+import team.unnamed.inject.Singleton;
 
+@Singleton
 public class HomePartModule extends AbstractModule {
 
-  
+  @Inject
+  private HomeListPart homeListPart;
 
   @Override
   public void configure() {
-
-
-
+    this.bindFactory(HomeModel.class, homeListPart);
   }
 }
