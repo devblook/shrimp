741ef4565a3d2e8661afc0e0946bf8d690a67ab2
jonakls <asmot54@hotmail.com>
2024-08-01 00:52:02 -0500

    chore(database): update name table

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -3,7 +3,7 @@ create table if not exists shrimp_homes
   home_id       varchar(36) primary key,
   home_name     varchar(255) not null,
   home_position varchar(255) not null,
-  created_at    timestamp    not null default current_timestamp
+  created_at    timestamp    not null
 );
 
 create table if not exists shrimp_users
@@ -17,7 +17,7 @@ create table if not exists shrimp_homes_users
   home_id varchar(36) not null,
   user_id varchar(36) not null,
   primary key (home_id, user_id),
-  foreign key (home_id) references shrimp_home (home_id),
+  foreign key (home_id) references shrimp_homes (home_id),
   foreign key (user_id) references shrimp_users (user_id)
 );
 
