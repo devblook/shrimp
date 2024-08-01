6a5f4ab8626358cad199bb6981d6b7497610051d
jonakls <asmot54@hotmail.com>
2024-08-01 00:52:37 -0500

    feat(manager): implements use of manager on join and quit of server

--- src/main/java/team/devblook/shrimp/listener/UserRegistryListener.java
+++ src/main/java/team/devblook/shrimp/listener/UserRegistryListener.java
@@ -4,26 +4,29 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import team.devblook.shrimp.manager.UserManager;
 import team.devblook.shrimp.model.UserModel;
-import team.devblook.shrimp.repository.TypeRepository;
 import team.unnamed.inject.Inject;
 
 public class UserRegistryListener implements Listener {
 
   @Inject
-  private TypeRepository<UserModel> userModelRepository;
+  private UserManager userManager;
 
   @EventHandler
   public void onUserRegistry(final PlayerJoinEvent event) {
     final Player player = event.getPlayer();
-
-    if (userModelRepository.get(player.getUniqueId()
-                                  .toString()) != null) {
-      return;
+    final UserModel userModel = userManager.findOne(player.getUniqueId());
+    if (userModel == null) {
+      throw new IllegalStateException("UserModel is null");
     }
+  }
 
-    final UserModel userModel = new UserModel(player.getUniqueId()
-                                                .toString());
-    userModelRepository.add(userModel);
+  @EventHandler
+  public void onUserSave(final PlayerQuitEvent event) {
+    final Player player = event.getPlayer();
+    final UserModel userModel = userManager.findOne(player.getUniqueId());
+    userManager.saveOne(userModel);
   }
 }
