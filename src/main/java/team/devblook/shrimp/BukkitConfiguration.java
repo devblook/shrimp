ec2eb9bb2dded198173f2574d8cb6c6975edda18
jonakls <asmot54@hotmail.com>
2024-08-11 00:39:06 -0500

    feat(files): add simple functions that allow obtaining values from configuration files

--- src/main/java/team/devblook/shrimp/BukkitConfiguration.java
+++ src/main/java/team/devblook/shrimp/BukkitConfiguration.java
@@ -1,5 +1,8 @@
 package team.devblook.shrimp;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -9,6 +12,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.util.Collections;
+import java.util.List;
 
 public class BukkitConfiguration {
 
@@ -55,5 +60,80 @@ public class BukkitConfiguration {
 
   public FileConfiguration get() {
     return this.configuration;
+  }
+
+  public Component getComponent(String path) {
+    return Component.text(this.get()
+                            .getString(path, path));
+  }
+
+  public Component getColoredComponent(final String path) {
+    final MiniMessage miniMessage = MiniMessage.miniMessage();
+    return miniMessage.deserialize(this.get()
+                                     .getString(path, path));
+  }
+
+  public Component getColoredComponent(String path, String... args) {
+    final MiniMessage miniMessage = MiniMessage.miniMessage();
+
+    if (args.length % 2 != 0) {
+      throw new IllegalArgumentException("Arguments must be in pairs");
+    }
+
+    String message = this.get()
+                       .getString(path, path);
+
+    for (int i = 0; i < args.length; i += 2) {
+      message = message.replace(args[i], args[i + 1]);
+    }
+
+    return miniMessage.deserialize(message)
+             .decorationIfAbsent(
+               TextDecoration.ITALIC,
+               TextDecoration.State.FALSE);
+  }
+
+  public List<Component> getColoredComponents(String path) {
+    final MiniMessage miniMessage = MiniMessage.miniMessage();
+    List<String> strings = this.get()
+                             .getStringList(path);
+
+    if (strings.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    return strings.stream()
+             .map(element -> miniMessage.deserialize(element)
+                               .decorationIfAbsent(
+                                 TextDecoration.ITALIC,
+                                 TextDecoration.State.FALSE))
+             .toList();
+  }
+
+  public List<Component> getColoredComponents(String path, String... args) {
+    final MiniMessage miniMessage = MiniMessage.miniMessage();
+
+    if (args.length % 2 != 0) {
+      throw new IllegalArgumentException("Arguments must be in pairs");
+    }
+
+    List<String> strings = this.get()
+                             .getStringList(path);
+
+    if (strings.isEmpty()) {
+      return Collections.emptyList();
+    }
+
+    return strings.stream()
+             .map(s -> {
+               for (int i = 0; i < args.length; i += 2) {
+                 s = s.replace(args[i], args[i + 1]);
+               }
+               return miniMessage.deserialize(s)
+                        .decorationIfAbsent(
+                          TextDecoration.ITALIC,
+                          TextDecoration.State.FALSE);
+             })
+             .toList();
   }
 }
